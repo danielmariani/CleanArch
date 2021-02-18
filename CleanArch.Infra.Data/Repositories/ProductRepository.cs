835ec19010fa5e9d3135429f73885fc967dce106
Daniel Mariani <eng.danielmariani@gmail.com>
2021-02-18 18:34:01 -0300

    Add project files.

---
+++ CleanArch.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using CleanArch.Infra.Data.Context;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class ProductRepository: IProductRepository
+    {
+        private ApplicationDbContext _context;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetById(int? id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public void Add(Product product)
+        {
+            _context.Add(product);
+            _context.SaveChanges();
+        }
+
+        public void update(Product product)
+        {
+            _context.Update(product);
+            _context.SaveChanges();
+        }
+
+        public void Remove(Product product)
+        {
+            _context.Remove(product);
+            _context.SaveChanges();
+        }
+
+
+    }
+}
---
+++ CleanArch.Infra.IoC/DependenceInjection.cs
@@ -0,0 +1,38 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.IoC
+{
+    public static class DependenceInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => 
+                options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+
+            return services;
+        }
+    }
+}
